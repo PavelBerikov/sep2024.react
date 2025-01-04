4b6f5dd4adbc64cbd465a6cbbd456701d971ca08
pavlosidukov <NoooB199505@gmail.com>
2025-01-04 23:33:03 +0200

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,10 @@
 import './App.css'
 import FamilyComponent from './components/FamilyComponent'
-
+import './components/simpsons.style.css'
 function App() {
   return (
     <>
         <FamilyComponent/>
-      /* Task 1 (kLZQR7Q98)
-        з arrays.js взяти масив simpsons.
-        Кореневим компонентом є App
-        Створити компонент CharacterComponent який буде містити всю інформацію щодо окремого об'єкту масиву. Розмітка компоненту довільна. Опис персонажу передати через children. Стилізація довільна, але обов'язково підключена і мінімально зроблена (хочаб один стиль, який працює).
-        Створити компонент FamilyComponent в якому буде відбуватись ітерація по масиву simpsons.*/
     </>
   )
 }
--- src/components/CharacterComponent.tsx
+++ src/components/CharacterComponent.tsx
@@ -1,18 +1,21 @@
 import { FC } from "react";
 import { ISimpson } from "../interfaces/simpson.iunterface";
 
+
 type SimpsonProps = {
     simpson: ISimpson
 }
 const CharacterComponent: FC<SimpsonProps> = ({simpson}) => {
     const {name, surname, age, info, photo} = simpson;
     return (
-        <div>
-            <h1>{name} {surname}</h1>
-            <h3>{age} y.o.</h3>
-            <p>{info}</p>
+        <div className="character">
             <img src={photo} alt={name}/>
-            <hr/>
+            <div>
+                <h1>{name} {surname}</h1>
+                <h3>{age} y.o.</h3>
+                <p>{info}</p>
+            </div>
+
         </div>
     );
 };
