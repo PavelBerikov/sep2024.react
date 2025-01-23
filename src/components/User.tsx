f69482826181828971f98863ba4248b5dda3765a
pavlosidukov <NoooB199505@gmail.com>
2025-01-23 12:16:34 +0200

    homework

--- src/components/User.tsx
+++ src/components/User.tsx
@@ -1,7 +1,8 @@
 import {FC, memo} from 'react';
 
+// функция мемо принимает компонент если проверхностное сравнение пропсов не выявило разницы, то компонент не перерендится
 const User:FC<{foo: () => void,  arr:number[] }> = memo((arr) => {
-    console.log('user')
+    console.log('User')
     console.log(arr)
     return (
         <div>
--- src/components/users/Users.tsx
+++ src/components/users/Users.tsx
@@ -5,10 +5,11 @@ const Users:FC = () => {
     // @ts-ignore
     const [users, setUsers] = useState([])
     console.log('users')
-
+    //сохраняем значением массива. Если депсы пусты, то массив отрендерится только 1 раз
     const arr:number[] = useMemo(() => {
         return [1, 2, 3, 4, 5, 6, 7]
     }, [])
+    // тоже самое что и юзМемо но для функций.
     const foo = useCallback(() => {
         console.log('test foo')
     }, [])
